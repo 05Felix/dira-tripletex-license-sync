024514c05788076c74eaf21e4c2719f11f9c7d08
Felix A <felixaasbo@F26Backup.localdomain>
2022-08-20 19:23:45 +0200

    v1

--- test.py
+++ test.py
@@ -1,16 +1,12 @@
 import requests
+import main
 
 base_url = "https://api.tripletex.io/v2"
 
-consumerToken = "eyJ0b2tlbklkIjoyMDEwLCJ0b2tlbiI6InRlc3QtNmY1NDRhZGMtODNmMy00NGRlLWE0MmEtY2EyMTBiZDdkNjYwIn0="
-employeeToken = "eyJ0b2tlbklkIjozMjc3LCJ0b2tlbiI6InRlc3QtYTNhODI5YzItNDA2Ny00NGIzLWEzMzktNjUxNTQ4ZmQyYjcwIn0="
-sessionToken = "eyJ0b2tlbklkIjoyMDI4NDQ4MSwidG9rZW4iOiJ0ZXN0LWIzNzEwZWFiLTQ3MzAtNGFkZC1hMWU4LTIzZWY1MzY3MTI4YSJ9"
-encodedToken = "MDpleUowYjJ0bGJrbGtJam95TURJNE5EUTRNU3dpZEc5clpXNGlPaUowWlhOMExXSXpOekV3WldGaUxUUTNNekF0TkdGa1pDMW" \
-               "hNV1U0TFRJelpXWTFNelkzTVRJNFlTSjk="
 
 konto_passord = "Test-1ICnm3EKy"
 
-auth = {"Authorization": f"Basic {encodedToken}"}
+auth = {"Authorization": f"Basic {main.encodedToken}"}
 
 ids = ["1279487", "1279489", "1279491"]
 
